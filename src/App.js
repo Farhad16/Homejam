3ce0b5efb1bb117826457b2ca7df05188d5830bb
farhad16 <fhossain389@gmail.com>
2021-05-17 07:07:54 +0600

    Upcoming shows design is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import HeaderContainer from './components/HeaderContainer/HeaderContainer';
+import UpcomingShows from './components/UpcomingShows/UpcomingShows';
 
 function App() {
   return (
     <div className="App">
       <HeaderContainer />
+      <UpcomingShows />
     </div>
   );
 }
