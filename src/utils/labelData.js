741193b0faaf615aa0b15427d7d294239173f32b
farhad16 <fhossain389@gmail.com>
2021-05-17 06:45:29 +0600

    Progressbar section is added

---
+++ src/utils/labelData.js
@@ -0,0 +1,30 @@
+import LoveIcon from '../assets/icons/love.svg';
+import StarIcon from '../assets/icons/star.svg';
+
+const labels = [
+  {
+    id: '1',
+    tag: 'abel',
+    value: 0,
+    icon: LoveIcon,
+  },
+  {
+    id: '2',
+    tag: 'label',
+    value: 0,
+    icon: StarIcon,
+  },
+  {
+    id: '3',
+    tag: 'label',
+    value: 0,
+    icon: StarIcon,
+  },
+  {
+    id: '4',
+    tag: 'label',
+    value: 0,
+    icon: StarIcon,
+  }
+];
+export default labels;
